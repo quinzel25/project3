dc1835bdfb437d4f4edd9a8bb0bcd991cdc521d4
quinz <quinzel0325@gmail.com>
2020-10-04 23:52:05 -0500

    working

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 from peewee import *
-
-db = SqliteDatabase('art.sqlite')
+from database_config import database_path
+#this creates the two tables and the database
+db = SqliteDatabase(database_path)
 
 class Artist(Model):
     artistID = AutoField()
-    name = CharField()
-    email = CharField()
+    name = CharField(unique= True)
+    email = CharField(unique= True)
     
     class Meta:
         database = db
@@ -25,94 +26,7 @@ db.connect()
 db.create_tables([Artist, Artwork])
 
 
-### Functions for database
 
-
-def add_artist(name_input, email_input):
-    try:
-        name_input = name_input.lower()
-        email_input = email_input.lower()
-        Artist.create(name=name_input, email=email_input, artistID = AutoField)
-        
-        print(f'{name_input} has been added into the Artist db')
-    except:
-        print('Error adding artist')
-
-def add_art(artist_input, artwork, price_input, status_input):
-
-    try:
-      
-        artist_get_id = Artist.get(Artist.name == artist_input).artistID
-        # print('artist ID: ', artist_get_id)
-        artist_get_id = int(artist_get_id)
-        Artwork.create( artist=artist_input, artName=artwork, price=price_input,status=status_input, artist_id= artist_get_id)
-
-        print(f'{artwork} added for {artist_input}')
-    except:
-        print('Error adding artpeice' )
-
-def does_artist_exist(name):
-    ###checks to see if artist is in database, returns a boolean
-    name = name.lower()
-
-    search = Artist.select().where(Artist.name == name)
-
-    if search:
-        return True
-    else:
-        return False
-
-def view_all_artists():
-    
-    query = Artist.select()
-    #print('fucntion called')
-    for x in query:
-        print(f'Artist: {x.name} | Email: {x.email} | Artist ID: {x.artistID}')
-
-
-def change_availability(art_id, new_status):
-
-    try:
-        new_status = new_status.lower().strip()
-        change = Artwork.update(status=new_status).where(Artwork.artwork_id == art_id)
-        change.execute()
-
-        print(f'Status changed to {new_status}')
-    except:
-        print('Error changing status')
-
-def all_art_by_artist(name):
-    try:
-        artist_id = get_artist_id(name)
-        name = name.lower()
-        query = Artwork.select().where(Artwork.artist_id == artist_id)
-      
-        print(f'All artwork from {name}: \n')
-        for x in query:
-            print(f'Art ID: {x.artwork_id} | Art name: {x.artName} | price: ${x.price} | Status: {x.status} ')
-    except:
-       print('Error retrieving art')
-
-def all_available_art(name):
-    try:
-        # query = Artwork.select().where(Artwork.status == 'available').join(Artist).where(Artist.name == name)
-        
-        artist_id = get_artist_id(name)
-        name = name.lower()
-        query = Artwork.select().where(Artwork.artist_id == artist_id, Artwork.status == 'available')
-
-        print(f'All available artwork from {name} : \n')
-        for x in query:
-            print(f'Art ID: {x.artwork_id} | Art name: {x.artName} | price: ${x.price} | Status: {x.status} ')
-    except:
-        print('Error retrieving art')
-
-def get_artist_id(artist_input):
-
-    artist_get_id = Artist.get(Artist.name == artist_input).artistID
-    artist_get_id = int(artist_get_id)
-    return artist_get_id
-        
 
 
 
--- ui.py
+++ ui.py
@@ -1,7 +1,10 @@
-import database
+import database_functions
+
+## this module handles the user inputs and returns the data along with some validation 
+
 
 def choice_one():
-    # 
+    
     name = input('Enter artist name: ')
     email = input('Enter artist email: ')
 
@@ -13,21 +16,21 @@ def choice_one():
 
 def choice_two(): 
     artist = input('Enter artist name: ')
-    #validation for checking if an artist is in the database
-    if database.does_artist_exist(artist):
+    #validation for checking if an artist is in the database_functions
+    if database_functions.does_artist_exist(artist):
         artwork = input('Enter art piece name: ')
         price = int(input('(integers only) Enter price: '))
         status = input('(available or sold) Enter status:  ')
 
         return artist, artwork, price, status
     else:
-        print('artist not in database')
+        print('artist not in database_functions')
         
         
 
 def get_artist_name():
     artist = input('Enter artist name: ')
-    if database.does_artist_exist(artist):
+    if database_functions.does_artist_exist(artist):
         return artist
     else:
         print('artist not in database')
@@ -35,7 +38,19 @@ def get_artist_name():
 
 def choice_six():
     art = int(input('Enter art piece id: '))
-    
-    new_status = input('New status of artwork (available or sold): ')
+    #input validation to makesure user doesnt type in random words for the availability
+    while True:
+        new_status = input('New status of artwork (available or sold): ')
+        if new_status.strip() == 'avaiable' or new_status.strip() == 'sold':
+            break
+        else:
+            print('must be \"available\" or \"sold\" ')
 
     return art, new_status
+
+def get_artwork_id():
+    #grabs id 
+    art = int(input('Enter art piece id to be delete: '))    
+    return art
+      
+    
